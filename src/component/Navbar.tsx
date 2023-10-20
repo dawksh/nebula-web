414adaeb88fab658c18803fd3aa563c255d9a614
Daksh <akshd@outlook.com>
2023-10-20 13:23:55 +0530

    chore: add navbar

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,18 @@
+
+import Image from 'next/image';
+import { ConnectKitButton } from 'connectkit';
+
+const Navbar = () => {
+    return (
+        <nav className="bg-black w-screen p-16 flex justify-between ">
+            <div className='mr-4'>
+                <Image src={"/assets/logo.png"} alt='Logo' height={100} width={100}></Image>
+            </div>
+            <div>
+                <span className="text-white px-4 py-2"><ConnectKitButton /></span>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
