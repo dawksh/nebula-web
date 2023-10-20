5f84ad274ef90033afc7f048013b057c8edc13a8
Daksh <akshd@outlook.com>
2023-10-20 12:32:11 +0530

    feat: add chains

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { WagmiConfig, createConfig } from "wagmi";
 import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+import { goerli, polygonMumbai, avalancheFuji } from 'wagmi/chains'
 
 const config = createConfig(
   getDefaultConfig({
@@ -10,10 +11,9 @@ const config = createConfig(
     walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
 
     // Required
-    appName: "Your App Name",
-
+    appName: "Nebula",
+    chains: [goerli, polygonMumbai, avalancheFuji],
     // Optional
-    appDescription: "Your App Description",
     appUrl: "https://family.co", // your app's url
     appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   }),
